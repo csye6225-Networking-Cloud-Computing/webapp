2e10dc4c7e041ededc65a00a5674c78d5b727d0f
Aayushi Choksi <choksi.aa@northeastern.edu>
2024-09-26 14:08:27 -0400

    Optimize health check endpoints
    Refactored HEAD and OPTIONS requests in app.js for efficiency. Enhanced health check logic in health.js for consistent response handling.

--- routes/health.js
+++ routes/health.js
@@ -1,39 +1,31 @@
 const express = require('express');
-
 const router = express.Router();
 
-//Health check endpoint for GET request
 router.get('/', (req, res) => {
-    //Check if the request contains query parameters
-    if (Object.keys(req.query).length > 0 || req.headers['content-length'] > 0) {
-        res.set({
-            'Cache-Control': 'no-cache, no-store, must-revalidate',
-            'Pragma': 'no-cache',
-            'X-Content-Type-Options': 'nosniff'
-        });
-        return res.status(400).send(); //Send 400 if query params or body payload is present
-    }
+  //Check for query parameters
+  if (Object.keys(req.query).length > 0) {
+    return res.status(400).send(); //400 Bad Request if query params are present
+  }
 
-    //Get all request headers
-    const requestHeaders = Object.keys(req.headers);
+  //Define allowed headers
+  const allowedHeaders = ['user-agent', 'accept', 'host', 'accept-encoding', 'connection', 'postman-token'];
+  const requestHeaders = Object.keys(req.headers);
 
-    //Check if any headers are present
-    if (requestHeaders.length > 0) {
-        res.set({
-            'Cache-Control': 'no-cache, no-store, must-revalidate',
-            'Pragma': 'no-cache',
-            'X-Content-Type-Options': 'nosniff'
-        });
-        return res.status(400).send(); //Send 400 if any headers are present
-    }
+  //Filter out disallowed headers
+  const disallowedHeaders = requestHeaders.filter(header => !allowedHeaders.includes(header));
 
-    //If no query params, body, or headers are present, return 200 OK
+  if (disallowedHeaders.length > 0) {
+    //Send 400 Bad Request if disallowed headers are present
     res.set({
-        'Cache-Control': 'no-cache, no-store, must-revalidate',
-        'Pragma': 'no-cache',
-        'X-Content-Type-Options': 'nosniff'
+      'Cache-Control': 'no-cache, no-store, must-revalidate',
+      'Pragma': 'no-cache',
+      'X-Content-Type-Options': 'nosniff'
     });
-    return res.status(200).send(); //Send 200 OK if everything is fine
+    return res.status(400).send(); 
+  }
+
+  //Send 200 OK if no disallowed headers and no query parameters
+  return res.status(200).send(); //200 OK
 });
 
 module.exports = router;
