51bab011d28ae095c04649d83b33d486083fb67d
Aayushi Choksi <choksi.aa@northeastern.edu>
2024-10-08 22:51:49 -0400

    changes

--- jest.setup.js
+++ jest.setup.js
@@ -1,4 +1,5 @@
 const dotenv = require('dotenv');
+const path = require('path');
 
-// Load environment variables from the .env file
-dotenv.config();
+// Load environment variables from the .env file explicitly
+dotenv.config({ path: path.resolve(__dirname, '../.env') });
