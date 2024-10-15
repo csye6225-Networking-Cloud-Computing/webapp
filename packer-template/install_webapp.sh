2ed0fff0be3da039adbaa6ed7b4ca7e0212eff8a
Aayushi Choksi <choksi.aa@northeastern.edu>
2024-10-15 19:27:09 -0400

    changes

--- packer-template/install_webapp.sh
+++ packer-template/install_webapp.sh
@@ -24,8 +24,8 @@ sudo systemctl start mysql
 echo "Setting up MySQL database..."
 sudo mysql -u root <<EOF
 CREATE DATABASE IF NOT EXISTS ${DB_NAME};
-CREATE USER IF NOT EXISTS '${DB_USER}'@'%' IDENTIFIED BY '${DB_PASSWORD}';
-GRANT ALL PRIVILEGES ON ${DB_NAME}.* TO '${DB_USER}'@'%';
+CREATE USER IF NOT EXISTS '${DB_USER}'@'localhost' IDENTIFIED WITH mysql_native_password BY '';
+GRANT ALL PRIVILEGES ON ${DB_NAME}.* TO '${DB_USER}'@'localhost';
 FLUSH PRIVILEGES;
 EXIT;
 EOF
